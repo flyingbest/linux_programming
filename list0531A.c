0cd7a2af94c6fafa2ffb565b059e5a3ab1c195da
flyingbest <taeyoon4715@gmail.com>
2015-10-04 15:48:38 +0900

    변수 스코프, 심볼 스코프

---
+++ list0531A.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a = 0;
+	{
+		a = 1;
+	}
+	printf("a = %d\n", a);
+	return 0;
+}
+
---
+++ list0531C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+static int a;
+int b;
+
+int main(){
+	a = 1;
+	b = 2;
+	printf("a+b = %d\n", a+b);
+	return 0;
+}
+
