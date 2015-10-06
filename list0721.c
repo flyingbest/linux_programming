54529d5402c35d38541a2a2293777b0e699dbff0
flyingbest <taeyoon4715@gmail.com>
2015-10-06 14:27:10 +0900

    data structure

---
+++ list0721.c
@@ -0,0 +1,17 @@
+#include <glib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	int i;
+	GQueue* queue = g_queue_new();
+
+	for(i = 0; i < argc; i++){
+		g_queue_push_head(queue, argv[i]);
+	}
+	
+	while(!g_queue_is_empty(queue)){
+		printf("%s\n", (char*) g_queue_pop_tail(queue));
+	}
+	return 0;
+}
+
